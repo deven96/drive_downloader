eaaf0fdcf9d9aeea818a709c0d14c42366b3f7a7
Deven96 <diretnandomnan@gmail.com>
2018-12-14 05:15:38 +0100

    functional package installed on test pypi

--- gdownloader/core/downloader.py
+++ gdownloader/core/downloader.py
@@ -8,7 +8,6 @@ import sys
 import urllib
 from urllib.request import urlopen, urlretrieve
 
-import numpy as np
 import pandas as pd
 import progressbar as pb
 
--- setup.py
+++ setup.py
@@ -5,14 +5,22 @@ import setuptools
 CURRENT_DIR = os.getcwd()
 REQUIREMENTS = 'requirements.txt'
 requires = [line.strip('\n') for line in open(REQUIREMENTS).readlines()]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setuptools.setup(
     name="GDownloader",
-    version="1.0.1",
+    version="0.0.7",
     author='Domnan Diretnan, Mmadu Manasseh',
+    author_email="diretnandomnan@gmail.com",
     description="easy wrapper for downloading google drive files using only shareable link",
+    url="https://github.com/deven96/drive_downloader",
     packages=setuptools.find_packages(),
-    zip_safe=False,
     install_requires=requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
